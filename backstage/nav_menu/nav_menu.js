02ae477cc5dfdcfa74af15dc271cf9ccf51cf6f0
wangdi <1490403440@qq.com>
2017-09-21 11:06:03 +0800

    不打开新页面

--- backstage/nav_menu/nav_menu.js
+++ backstage/nav_menu/nav_menu.js
@@ -12,18 +12,18 @@ function nav_menu_output(output_id) {
     '    <div class  =  "col-lg-12 nav_col">'+
     '      <div class = "list-group">'+
     '        <a class = "list-group-item clearfix active nav_radius">用户管理<span class = "glyphicon glyphicon-chevron-down pull-right"></span></a>'+
-    '        <a href = "../index/user_list.html" target = "_blank" class = "list-group-item clearfix nav_pl30">用户列表</a>'+
+    '        <a href = "../index/user_list.html" class = "list-group-item clearfix nav_pl30">用户列表</a>'+
     '        <a class = "list-group-item clearfix active nav_radius">首页面数据<span class = "glyphicon glyphicon-chevron-down pull-right"></span></a>'+
-    '        <a href = "../index/price_change.html" target = "_blank" class = "list-group-item clearfix nav_pl30">价格变动</a>'+
-    '        <a href = "../index/dynamic_data.html" target = "_blank" class = "list-group-item clearfix nav_pl30">动态数据</a>'+
+    '        <a href = "../index/price_change.html" class = "list-group-item clearfix nav_pl30">价格变动</a>'+
+    '        <a href = "../index/dynamic_data.html" class = "list-group-item clearfix nav_pl30">动态数据</a>'+
     '        <a class = "list-group-item clearfix active">油品数据管理<span class = "glyphicon glyphicon-chevron-down pull-right"></span></a>'+
     '        <a class = "list-group-item clearfix nav_pl30 nav_bgddd">找油商城/现货搜索<span class = "glyphicon glyphicon-chevron-down pull-right nav_colorfff"></span></a>'+
 
     '        <a href = "../index/data_list.html" target = "_blank" class = "list-group-item clearfix nav_pl45">添加数据</a>'+
     '        <a href = "../index/data_list.html" target = "_blank" class = "list-group-item clearfix nav_pl45">数据列表</a>'+
     '        <a class = "list-group-item clearfix active">油品委托<span class = "glyphicon glyphicon-chevron-down pull-right"></span></a>'+
-    '        <a href = "../index/entrust_data_list.html" target = "_blank" class = "list-group-item clearfix nav_pl30">委托需求</a>'+
-    '        <a href = "../index/demand_data_list.html" target = "_blank" class = "list-group-item clearfix nav_pl30">委托信息</a>'+
+    '        <a href = "../index/entrust_data_list.html" class = "list-group-item clearfix nav_pl30">委托需求</a>'+
+    '        <a href = "../index/demand_data_list.html" class = "list-group-item clearfix nav_pl30">委托信息</a>'+
     '        <a class = "list-group-item active text-center nav_radius">Copyright &copy; 2017</a>'+
     '      </div>'+
     '    </div>'+
